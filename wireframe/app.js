1618d4dae13d6bf14168a6c639641fc9b3758269
qaijuana <qai.ableman@gmail.com>
2021-09-21 15:36:14 +0800

    basic conditions of the game is set. Moving on to appending them onscreen

--- wireframe/app.js
+++ wireframe/app.js
@@ -4,21 +4,33 @@ const keystroke =
 
 
 
-const userCard = {
-    "name": null,
-    "currentScore": null,
-    "accuracy": null,
-}
+
 
 
 
 $(() => {
     //! CALLING FUNCTIONS WOULD BE INSIDE HERE.
 
+    const user = {
+        "name": null,
+        "currentScore": 0,
+        "currentChoice": -1,
+        "currentLight": null,
+        "currentStreak": 0,
+        "accuracy": null,
+        "highStreak": 0,
+        "highScore": 0,
+    }
+    
+    const ai = {
+        "name": "Dexter",
+        "currentScore": 0,
+        "currentChoice": -1
+    } 
 
 
 
-    //!GAME RULES 
+    //!GAME MECHANISM IS BELOW 
 
 
     // console.log($('.bttn'))
@@ -32,76 +44,87 @@ $(() => {
     // console.log($bttnArray)
     // console.log($buttonCheck);
     // console.log(keystroke)
-
-
-
-
+    
+    
+    
+    
     const userTyping =
-        $('body').on("keydown", e => {
-            const keyNormalize = e.key.toLowerCase();
-            const userIndex = keystroke.indexOf(e.key.toLowerCase());
-            //! Condition: if div matches targetGen it will light up. 
-            //? Visuals for User to see as guide
-            const userChoice = $(".bttn").map(function () {
-                const userTrigger = this.innerText.toLowerCase()
-                 if (userTrigger === keyNormalize) {
-                    $lightUp(this);
-                    console.log ("userIndex: " + userIndex)
-                    console.log("userChoice: " + userTrigger)
-                    // console.log( userIndex )
-                    return userIndex
-                }
-            })
-        // return (userIndex);   sod 
-            
+    $('body').on("keydown", e => {
+        const keyNormalize = e.key.toLowerCase();
+        const userIndex = keystroke.indexOf(e.key.toLowerCase());
+        //! Condition: if div matches targetGen it will light up. 
+        //? Visuals for User to see as guide
+        const userChoice = $(".bttn").map(function () {
+            const userTrigger = this.innerText.toLowerCase()
+            if (userTrigger === keyNormalize) {
+                $lightUp(this);
+                // console.log ("userIndex: " + userIndex)
+                // console.log("userChoice: " + userTrigger)
+                // console.log( userIndex )
+                user.currentLight = this
+                user.currentChoice = userIndex
+                console.log(user.currentChoice)
+                compareChoice(user.currentLight)
+                return userIndex
+            }
+        })
+        
+        
+    }); 
+    
+    $('.bttn').on("click", e => {
+        $lightUp(e.currentTarget)
+        const mouseChoice = e.currentTarget.innerText.toLowerCase()
+        const mouseIndex = keystroke.indexOf(mouseChoice)
+        // console.log("mouseChoice: " + mouseChoice)
+        // console.log("mouseIndex: " + mouseIndex)
+        return mouseIndex
     });
 
-
-
-    
-    //? targetGen makes a random selection for keystroke for user to hit with the key within a set time
-
-    
-
+    //! Condition: if div matches targetGen it will light up. User will now have to follow through to hit the light target.
 
     theAI = () => {
 
-        const targetGen = 
-        keystroke [Math.floor( (Math.random() * (keystroke.length) ))];
-    
+        //? targetGen makes a random selection for keystroke for user to hit with the key within a set time
+
+        const targetGen = Math.floor( (Math.random() * (keystroke.length) ));
+
         //! This followed by a condition to run $lightUp when matched with targetGen condition 
-        const aiBrain = $(".bttn").map(function () {
+        // const test = $('.bttn').map( x => {
+        //     console.log (x)
+        // })
+
+        const aiBrain = $(".bttn").map( function() {
+            // console.log (this)
             const aiChoice = this.innerText.toLowerCase();
-            const aiIndex = $bttnArray.indexOf(targetGen);
-            
-            //! Condition: if div matches targetGen it will light up. User will now have to follow through to hit the light target.
-            if ( aiChoice === targetGen) {
-                $aiLightUp(this)
-                console.log("aiChoice: " + aiChoice)
-                console.log("aiIndex: " + aiIndex)
-                return (aiIndex)
+            const aiIndex = $bttnArray.indexOf(aiChoice);
+            // console.log(aiIndex)
+            if ( aiIndex === targetGen) {
+            $aiLightUp(this)
+            // console.log("aiIndex: " + aiIndex)
+            // console.log("aiChoice: " + aiChoice)
+            ai.currentChoice = aiIndex
+            console.log(ai.currentChoice)
+            return aiIndex
             }   
+
         })
+
+    return ( aiBrain )
     };
 
-
-    const theRules = () => {
-        console.log(theAI)
-        console.log(userTyping)
-
-    }
-
-    // theRules();
-
-
-
-
-
-
     //!
-    //! this guy is generating a random index per 500ms
+    //! this guy is producing intervals 
     //!
-    const aiSpeed = setInterval(theAI, 1000)
+    const aiSpeed = setInterval(theAI, 700)
+
+    outOfTime = () => {
+        return clearInterval(aiSpeed)
+    };
+
+    //! Temporary timer
+    setTimeout(outOfTime, 60_000)
+
     // const gameSpeed = setInterval(timerDisplay ,1000)
 
     //! timerDisplay() is supposed to append a timer into html (Not done)
@@ -110,9 +133,6 @@ $(() => {
     //! Function to stop count
     ////
 
-    outOfTime = () => {
-        return clearInterval(aiSpeed)
-    };
 
     clockCount = () => {
         return clearInterval(gameSpeed)
@@ -123,24 +143,57 @@ $(() => {
     //!
     // setTimeout(outOfTime, 45_000)
 
-    setTimeout(outOfTime, 10_000)
     // setTimeout(clockCount, 60_000)
 
 
 
+
+
+    ////
+    //! GAME RULES AND CONDITIONS 
+    ////
+    const missed = [];
+    compareChoice = (light) => {
+        
+        if (user.currentChoice === ai.currentChoice) {
+            $scoreLightUp(light)
+            user.currentScore = user.currentScore + 1
+            user.currentStreak += 1
+            console.log("current Score " + user.currentScore)
+            console.log("current streak " + user.currentStreak)
+            console.log("sick")
+        } else {
+            user.currentStreak = 0
+            missed.push(ai.currentChoice)
+            console.log("current Score " + user.currentScore)
+            console.log("current streak " + user.currentStreak)
+            // console.log(missed)
+            console.log("suck")
+        } 
+        if (missed.length > 3) {
+            for (let i = 0; i < missed.length; i++) {
+                missed.pop()
+            }
+            console.log("missed: " + missed)
+            $missLightUp(light)
+            user.currentScore -= 5
+            console.log("current Score " + user.currentScore)
+            console.log("current streak " + user.currentStreak)
+        }
+        if (user.currentStreak > 20) {
+            user.currentScore += 2
+        }
+        
+        
+        
+        console.log("missed: " + missed)
+    }
     
 
-    // console.log(userTyping)
 
-    $('.bttn').on("click", e => {
-        $lightUp(e.currentTarget)
-        const mouseChoice = e.currentTarget.innerText.toLowerCase()
-        const mouseIndex = keystroke.indexOf(mouseChoice)
-        // console.log("mouseChoice: " + mouseChoice)
-        // console.log("mouseIndex: " + mouseIndex)
-        return mouseIndex
-    });
 
+
+    //! THIS SECTION BELOW IS ALL ABOUT LIGTING THE SCREEN UP
 
     ////
     //! This shows where AI or User is at on screen.
@@ -160,7 +213,7 @@ $(() => {
         setInterval(() => {
             e.classList
                 .remove('activeAI')
-        }, 600);
+        }, 800);
     }
 
 
@@ -179,7 +232,7 @@ $(() => {
         setInterval(() => {
             e.classList
                 .remove('activeMiss')
-        }, 800);
+        }, 500);
     }
 
 });
